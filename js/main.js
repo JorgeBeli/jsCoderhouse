f32db9e0e8a588561e42e8775429048ea30cf5a5
JorgeBeli <105600872+JorgeBeli@users.noreply.github.com>
2022-07-13 22:29:28 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const describir = (palabra) =>{
+    palabra = prompt('ingrese su palabra')
+    if (isNaN(palabra)&& palabra !== null && palabra!==''){
+        let vocales=0;
+        for (let i=0; i<palabra.length;i++){
+            if (palabra[i]==='a'||palabra[i]==='e'||palabra[i]==='i'||palabra[i]==='o'||palabra[i]==='u'){
+                vocales +=1
+            }
+        }
+        return `Tu palabra tiene un total de ${vocales} vocales`;
+    }else {
+        alert(`Ingresa una palabra no: '${palabra}' >:C`);
+    }
+}
+describir()
